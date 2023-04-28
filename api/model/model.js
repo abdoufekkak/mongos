1da914dd36c28cb7fd34d19373e213477113e222
oussama toumi <toumi11oussama@gmail.com>
2023-04-28 17:45:15 +0100

    a

--- api/model/model.js
+++ api/model/model.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
-<<<<<<< HEAD:api/model/model.js
+
 const PersonnneShema = mongoose.Schema({
-=======
-const personne = mongoose.Schema({
->>>>>>> 94b28bc2d7ed7711862a27d95b12b4b410ae1086:api/model.js
+
   Cni: {
     type: String,
   },
@@ -35,8 +33,6 @@ const personne = mongoose.Schema({
   },
 });
 
-<<<<<<< HEAD:api/model/model.js
+
 export const product = mongoose.model("Personnne", PersonnneShema);
-=======
-export const product = mongoose.model("Product", personne);
->>>>>>> 94b28bc2d7ed7711862a27d95b12b4b410ae1086:api/model.js
+
